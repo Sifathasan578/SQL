5b190e1c4c359eabfa9cf0c9e86ebfef2ac989c1
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-07-04 04:23:07 +0600

    Update African Cities.sql

--- HackerRank/African Cities.sql
+++ HackerRank/African Cities.sql
@@ -1,7 +1,29 @@
+-- Topic: Basic Join, Nested Queries 
+
 select city.name
 from city
 where city.countrycode in (
     select country.code
     from country
     where country.continent = 'Africa'
+);
+
+
+-- Others: 
+
+-- MySQL The first:
+
+Select City.Name From City Inner Join Country 
+On CountryCode = Code
+Where Continent = 'Africa';
+
+-- The second:
+
+Select City.Name From City, Country
+Where Code = CountryCode And Continent = 'Africa';
+
+-- The third:
+
+Select Name From City Where CountryCode In (
+    Select Code From Country Where Continent = 'Africa'
 );
