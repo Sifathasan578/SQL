27bfc8ad327bca03253b5df1ec3317dcff73ec7c
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-07-04 19:51:17 +0600

    Update Top Earners.sql

--- HackerRank/Top Earners.sql
+++ HackerRank/Top Earners.sql
@@ -5,3 +5,16 @@ from employee
 group by salary * months
 order by salary * months desc
 limit 1;
+
+-- Pending: 
+SELECT 
+    MAX(total_earnings) AS max_earnings, 
+    COUNT(*) AS count_of_max_earners
+FROM (
+    SELECT salary * months AS total_earnings
+    FROM employeee
+) AS earnings
+WHERE total_earnings = (
+    SELECT MAX(salary * months) 
+    FROM employeee
+);
